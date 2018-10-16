16098bd19dbb98772a6f8f6a8a097e0a3000ba9e
socamporomani <socamporomani@nautilus10.danielcastelao.org>
2018-10-16 09:28:55 +0200

    comentario con abc

--- src/exemploarearectangulo/ExemploAreaRectangulo.java
+++ src/exemploarearectangulo/ExemploAreaRectangulo.java
@@ -34,3 +34,6 @@ public class ExemploAreaRectangulo {
     }
     
 }
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaA
+        Bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+ccccccccccccccccccccccc

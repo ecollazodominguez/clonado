8be6d7150f50c2f87f6c76bdd57052362fa7ebb8
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2018-10-16 08:50:18 +0200

    prueba

---
+++ src/exemploarearectangulo/ExemploAreaRectangulo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemploarearectangulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class ExemploAreaRectangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner ler = new Scanner(System.in);
+        
+        float base= 0, altura= 0, area= 0;
+        System.out.println("teclea base:");
+        base=ler.nextFloat();
+        System.out.println("teclea altura:");
+        altura=ler.nextFloat();
+        area= base*altura;
+    
+        System.out.println("Area rectángulo = "+area );
+        //Tamen se pode facer
+//        float base= 4.3f, altura= 3f;
+//        System.out.println("Area rectángulo = "+base*altura );
+        
+    }
+    
+}
